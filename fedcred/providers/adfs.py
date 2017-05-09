090e00ee216c839b67fbd06ea68e1e3fd9c52ba6
Zach Zeid <zach.zeid@gmail.com>
2017-05-09 14:00:01 -0400

    Add support for NTLM authentication in front of ADFS

--- fedcred/providers/adfs.py
+++ fedcred/providers/adfs.py
@@ -4,6 +4,7 @@ except ImportError:
     from ConfigParser import NoOptionError, NoSectionError
 import re
 import requests
+from requests_ntlm import HttpNtlmAuth
 import sys
 
 from bs4 import BeautifulSoup
@@ -17,6 +18,7 @@ class Adfs(object):
             self.sslverification = self.config.getboolean(
                 common.DEFAULT_CONFIG_SECTION, 'sslverify')
             self.idpurl = self.config.get('adfs', 'url')
+	    self.ntlmauth = self.config.get('adfs', 'ntlmauth')
         except (NoOptionError, NoSectionError) as e:
             sys.exit(e.message)
 
@@ -24,48 +26,93 @@ class Adfs(object):
         username, password = common.get_user_credentials()
 
         session = requests.Session()
-
-        try:
-            form_response = session.get(
-                self.idpurl, verify=self.sslverification)
-            formsoup = BeautifulSoup(form_response.text, "html.parser")
-            payload_dict = {}
-            for inputtag in formsoup.find_all(re.compile('(INPUT|input)')):
-                name = inputtag.get('name', '')
-                value = inputtag.get('value', '')
-                if "user" in name.lower():
-                    payload_dict[name] = username
-                elif "pass" in name.lower():
-                    payload_dict[name] = password
-                else:
+	if self.ntlmauth:
+            try:
+                form_response = session.get(self.idpurl, verify=self.sslverification, auth=HttpNtlmAuth(username, password))
+                formsoup = BeautifulSoup(form_response.text, "html.parser")
+                payload_dict = {}
+                for inputtag in formsoup.find_all(re.compile('(INPUT|input)')):
+                    name = inputtag.get('name', '')
+                    value = inputtag.get('value', '')
+                    if "user" in name.lower():
+                        payload_dict[name] = username
+                    elif "pass" in name.lower():
+                        payload_dict[name] = password
+                    else:
                     # Simply populate the parameter with the existing value
                     # (picks up hidden fields in the login form)
-                    payload_dict[name] = value
-            for inputtag in formsoup.find_all(re.compile('(FORM|form)')):
-                action = inputtag.get('action')
+                        payload_dict[name] = value
+                for inputtag in formsoup.find_all(re.compile('(FORM|form)')):
+                    action = inputtag.get('action')
             # parsedurl = urlparse(idpentryurl)
             # idpauthformsubmiturl = "{scheme}://{netloc}{action}".format(
             #                         scheme=parsedurl.scheme,
             #                         netloc=parsedurl.netloc,
             #                         action=action)
-            response = session.post(action, data=payload_dict,
+                response = session.post(action, data=payload_dict,
                                     verify=self.sslverification)
-            if response.status_code != 200:
-                sys.exit('There was a problem logging in via ADFS. HTTP '
+                if response.status_code != 200:
+                    sys.exit('There was a problem logging in via ADFS. HTTP '
                          'Status Code: %s' % (response.status_code))
 
-            assertion = common.get_saml_assertion(response)
-            arn_to_assume = common.get_arns_from_assertion(assertion)
-            sts_creds = common.get_sts_creds(arn_to_assume)
-            try:
-                common.write_credentials(
+                assertion = common.get_saml_assertion(response)
+                arn_to_assume = common.get_arns_from_assertion(assertion)
+                sts_creds = common.get_sts_creds(arn_to_assume)
+                try:
+                    common.write_credentials(
                     self.config.get(
                         common.DEFAULT_CONFIG_SECTION,
                         'aws_credential_profile'
                     ),
                     sts_creds
                     )
-            except (NoOptionError, NoSectionError) as e:
-                sys.exit(e.message)
-        except requests.exceptions.ConnectionError as e:
-            sys.exit('Could not connect to %s. %s' % (self.idpurl, e))
+                except (NoOptionError, NoSectionError) as e:
+                    sys.exit(e.message)
+            except requests.exceptions.ConnectionError as e:
+                sys.exit('Could not connect to %s. %s' % (self.idpurl, e))
+		
+	else:
+            try:
+                form_response = session.get(
+                self.idpurl, verify=self.sslverification)
+                formsoup = BeautifulSoup(form_response.text, "html.parser")
+                payload_dict = {}
+                for inputtag in formsoup.find_all(re.compile('(INPUT|input)')):
+                    name = inputtag.get('name', '')
+                    value = inputtag.get('value', '')
+                    if "user" in name.lower():
+                        payload_dict[name] = username
+                    elif "pass" in name.lower():
+                        payload_dict[name] = password
+                    else:
+                    # Simply populate the parameter with the existing value
+                    # (picks up hidden fields in the login form)
+                        payload_dict[name] = value
+                for inputtag in formsoup.find_all(re.compile('(FORM|form)')):
+                    action = inputtag.get('action')
+            # parsedurl = urlparse(idpentryurl)
+            # idpauthformsubmiturl = "{scheme}://{netloc}{action}".format(
+            #                         scheme=parsedurl.scheme,
+            #                         netloc=parsedurl.netloc,
+            #                         action=action)
+                response = session.post(action, data=payload_dict,
+                                    verify=self.sslverification)
+                if response.status_code != 200:
+                    sys.exit('There was a problem logging in via ADFS. HTTP '
+                         'Status Code: %s' % (response.status_code))
+
+                assertion = common.get_saml_assertion(response)
+                arn_to_assume = common.get_arns_from_assertion(assertion)
+                sts_creds = common.get_sts_creds(arn_to_assume)
+                try:
+                    common.write_credentials(
+                        self.config.get(
+                        common.DEFAULT_CONFIG_SECTION,
+                        'aws_credential_profile'
+                    ),
+                    sts_creds
+                    )
+                except (NoOptionError, NoSectionError) as e:
+                    sys.exit(e.message)
+            except requests.exceptions.ConnectionError as e:
+                sys.exit('Could not connect to %s. %s' % (self.idpurl, e))
