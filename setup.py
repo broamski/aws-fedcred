67a95688edf7bd827590707c9df9f3106ae316ce
broamski <brian.nuszkowski@me.com>
2017-05-10 08:17:52 -0400

    bump to 0.0.2, fix pep error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fedcred',
-    version='0.0.1',
+    version='0.0.2',
     description='Get AWS API Credentials When using an '
                 'Identity Provider/Federation',
     author='Brian Nuszkowski',
@@ -10,6 +10,6 @@ setup(
     scripts=['bin/fedcred'],
     packages=find_packages(),
     url='https://github.com/broamski/aws-fedcred',
-    install_requires=['requests_ntlm>=1.0.0', 'beautifulsoup4>=4.4.1', 'boto3>=1.2.3',
-                      'requests>=2.8.1']
+    install_requires=['beautifulsoup4>=4.4.1', 'boto3>=1.2.3',
+                      'requests>=2.8.1', 'requests_ntlm>=1.0.0']
 )
